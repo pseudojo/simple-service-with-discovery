8dbed211f58454006ffa4fe48a57e8207c6076bb
비디 조현구 <hgcho@bluedigm.com>
2018-01-25 10:39:34 +0900

    Add default source codes.

---
+++ src/main/java/com/example/simpleservicewithdiscovery/SimpleServiceWithDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package com.example.simpleservicewithdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleServiceWithDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleServiceWithDiscoveryApplication.class, args);
+	}
+}
